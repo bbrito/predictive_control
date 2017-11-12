dd5122e3bd5eddee8c7c148c23dbd13cc0fa99fd
ipa-mjp <mayank.jitendrakumar.patel@ipa.fraunhofer.de>
2017-11-12 19:02:56 +0100

    corrected velocty function, make same as position functions

--- src/predictive_trajectory_generator.cpp
+++ src/predictive_trajectory_generator.cpp
@@ -64,28 +64,27 @@ void predictive_config::choose_discretization_steps()
 
 void predictive_config::enforce_position_limit(double current_position, double& corrected_position)
 {
-	// Current position inside the range.
-	if ( (current_position - min_position_limit > 0) || (min_position_limit + position_tolerance < current_position) )
+	ROS_DEBUG_STREAM("Maximum position limit with tolerance(max_position_limit + position_tolerance): "<< (max_position_limit+position_tolerance));
+	ROS_DEBUG_STREAM("Manimum position limit with tolerance(min_position_limit - position_tolerance): "<< (min_position_limit-position_tolerance));
+	ROS_DEBUG_STREAM("Absolute value of current position - min_position_limit: "<<std::abs(current_position - min_position_limit));
+	ROS_DEBUG_STREAM("Absolute value of current position - max_position_limit: "<<std::abs(max_position_limit - current_position));
+
+	// Current position is within defined minimum and maximum position limits
+	if ( ((min_position_limit-position_tolerance) - current_position <0.0) &&  (current_position - (max_position_limit+position_tolerance) < 0.0))
 	{
 		corrected_position = current_position;
 	}
 
 	// Current position outside the range, and also near to minimum limit.
-	else if ( ((current_position - min_position_limit < 0)  || (min_position_limit + position_tolerance > current_position))
+	else if ( ((min_position_limit-position_tolerance) >= current_position )
 				&& (std::abs(current_position - min_position_limit) ) < std::abs(max_position_limit - current_position) )
 	{
 		corrected_position = min_position_limit;
 	}
 
-	// Current position inside the range.
-	else if ( (  max_position_limit - current_position  > 0) || (max_position_limit - position_tolerance > current_position) )
-	{
-		corrected_position = current_position;
-	}
-
 	// Current position outside the range, and also near to maximum limit.
-	else if ( ((max_position_limit - current_position  < 0)  || (max_position_limit - position_tolerance < current_position))
-				&& (std::abs(current_position - min_position_limit) ) > std::abs(max_position_limit - current_position) )
+	else if ( ((max_position_limit+position_tolerance) <= current_position)
+			&& ((std::abs(current_position - min_position_limit) ) > std::abs(max_position_limit - current_position)) )
 	{
 		corrected_position = max_position_limit;
 	}
@@ -129,6 +128,7 @@ bool predictive_config::check_position_tolerance_violation(double current_positi
 	return false;
 }
 
+/*
 void predictive_config::enforce_velocity_limit(double current_velocity, double& corrected_velocity)
 {
 	// Current position inside the range.
@@ -162,7 +162,41 @@ void predictive_config::enforce_velocity_limit(double current_velocity, double& 
 		ROS_WARN("No velocity limit satisfied, need to check code and limit ... enforce_velocity_limit");
 	}
 }
+*/
+void predictive_config::enforce_velocity_limit(double current_velocity, double& corrected_velocity)
+{
+	ROS_DEBUG_STREAM("Maximum velocity limit with tolerance(max_velocity_limit + velocity_tolerance): "<< (max_velocity_limit+velocity_tolerance));
+	ROS_DEBUG_STREAM("Manimum velocity limit with tolerance(min_velocity_limit - velocity_tolerance): "<< (min_velocity_limit-velocity_tolerance));
+	ROS_DEBUG_STREAM("Absolute value of current velocity - min_velocity_limit: "<<std::abs(current_velocity - min_velocity_limit));
+	ROS_DEBUG_STREAM("Absolute value of current velocity - max_velocity_limit: "<<std::abs(max_velocity_limit - current_velocity));
 
+	// Current velocity is within defined minimum and maximum position limits
+	if ( ((min_velocity_limit-velocity_tolerance) - current_velocity <0.0) &&  (current_velocity - (max_velocity_limit+velocity_tolerance) < 0.0))
+	{
+		corrected_velocity = current_velocity;
+	}
+
+	// Current velocity outside the range, and also near to minimum limit.
+	else if ( ((min_velocity_limit-velocity_tolerance) >= current_velocity )
+				&& (std::abs(current_velocity - min_velocity_limit) ) < std::abs(max_velocity_limit - current_velocity) )
+	{
+		corrected_velocity = min_velocity_limit;
+	}
+
+	// Current velocity outside the range, and also near to maximum limit.
+	else if ( ((max_velocity_limit+velocity_tolerance) <= current_velocity)
+			&& ((std::abs(current_velocity - min_velocity_limit) ) > std::abs(max_velocity_limit - current_velocity)) )
+	{
+		corrected_velocity = max_velocity_limit;
+	}
+
+	else
+	{
+		ROS_WARN("No velocity limit satisfied, need to check code and limit");
+	}
+}
+
+/*
 bool predictive_config::check_velocity_tolerance_violation(double current_velocity)
 {
 	if ( current_velocity - (min_velocity_limit - velocity_tolerance) < 0)
@@ -178,6 +212,40 @@ bool predictive_config::check_velocity_tolerance_violation(double current_veloci
 		ROS_WARN("Velocity tolerance violate with current velocity %f required velocity %f ... check_velocity_tolerance_violation",
 				current_velocity, (max_velocity_limit + velocity_tolerance) );
 		return true;
+	}
+
+	return false;
+}
+*/
+
+bool predictive_config::check_velocity_tolerance_violation(double current_velocity)
+{
+	ROS_DEBUG_STREAM("Current velocity: "<<current_velocity);
+	ROS_DEBUG_STREAM("Difference value current - (min limit - tol): " << current_velocity - (min_velocity_limit - velocity_tolerance));
+	ROS_DEBUG_STREAM("Difference value current (max limit + tol): " <<current_velocity - (max_velocity_limit + velocity_tolerance));
+
+	// Current velocity is below than minimum velocity limit + tolerance
+	if ( (current_velocity + (min_velocity_limit - velocity_tolerance)) < 0.0)
+	{
+		position_tolerance_violate = true;
+		ROS_WARN("Velocity tolerance violate with current position %f required position %f ... check_velocity_tolerance_violation",
+				current_velocity, (min_velocity_limit - velocity_tolerance) );
+		return true;
+	}
+
+	// Current velocity is above than maximum velocity limit + tolerance
+	else if ( (current_velocity - (max_velocity_limit + velocity_tolerance)) >= 0.0)
+	{
+		position_tolerance_violate = true;
+		ROS_WARN("Velocity tolerance violate with current position %f required position %f ... check_velocity_tolerance_violation",
+				current_velocity, (max_velocity_limit + velocity_tolerance) );
+		return true;
+	}
+
+	// Current velocity is within range of minimum and maximum velocity limit
+	else if (min_velocity_limit < current_velocity < max_velocity_limit)
+	{
+		ROS_INFO("Current velocity is within range of minimum and maximum velocity limit ... predictive_config::check_velocity_tolerance_violation");
 	}
 
 	return false;

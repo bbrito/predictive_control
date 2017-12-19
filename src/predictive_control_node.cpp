7742b0145ac55fedbf781cb450b6e42d8aa6dad5
ipa-mjp <mayank.jitendrakumar.patel@ipa.fraunhofer.de>
2017-12-19 13:01:46 +0100

    call predictive_control node

--- src/predictive_control_node.cpp
+++ src/predictive_control_node.cpp
@@ -1,12 +1,15 @@
 
 #include <ros/ros.h>
+#include <predictive_control/predictive_controller.h>
 
 int main(int argc, char **argv)
 {
 	try
 	{
 		ros::init(argc, argv, ros::this_node::getName());
-
+		predictive_control_node pd_control_node;
+		pd_control_node.main_predictive_control();
+		ros::spin();
 	}
 
 	catch (ros::Exception& e)

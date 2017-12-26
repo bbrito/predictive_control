f17e835bb30a63cc2f18604987bfb10c8272bd75
ipa-mjp <mayank.jitendrakumar.patel@ipa.fraunhofer.de>
2017-12-26 19:22:39 +0100

    new test file

---
+++ test/new_kinematic_calculation_test.cpp
@@ -0,0 +1,35 @@
+
+#include <ros/ros.h>
+
+#include <predictive_control/kinematic_calculations.h>
+
+int main(int argc, char **argv)
+{
+	try
+	{
+		ros::init(argc, argv, "kinematic_test");
+		ros::NodeHandle node_handler;
+
+		if (node_handler.hasParam("/robot_description"))
+		{
+			Kinematic_calculations kin_solver;
+			kin_solver.initialize();
+
+			ROS_INFO("Done");
+		}
+
+		else
+		{
+			ROS_ERROR("Robot_description not available");
+			exit(1);
+		}
+
+	} catch (ros::Exception& e)
+
+	{
+		ROS_ERROR("%s", e.what());
+		exit(1);
+	}
+
+return 0;
+}

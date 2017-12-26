f288829e14de1e6c40cf8f2783b95854fc8e0bbc
ipa-mjp <mayank.jitendrakumar.patel@ipa.fraunhofer.de>
2017-12-26 18:32:28 +0100

    corrected new functions

--- include/predictive_control/kinematic_calculations.h
+++ include/predictive_control/kinematic_calculations.h
@@ -35,129 +35,73 @@
 // acado includes
 #include <acado_optimal_control.hpp>
 
+#include<predictive_control/predictive_configuration.h>
+
 #define _DEBUG_  false
 
-class Kinematic_calculations
-	{
-	private:
+class Kinematic_calculations: public predictive_configuration
+{
+public:
+  Kinematic_calculations();
+  ~Kinematic_calculations();
 
-		unsigned int segments;
-		unsigned int dof;
+  /**
+   * @brief initialize: initialize kinematic chain and urdf model
+   * @param rbt_description: urdf model of robot description
+   * @return success if successful initialization otherwise false
+   */
+  bool initialize(const std::string rbt_description = "/robot_description");
 
-		std::string base_link;
-		std::string tip_link;
-		std::string root_frame;
+  /**
+   * @brief calculate_inverse_jacobian_bySVD: calculate inverse of Jacobian Matrix using Singular Value Decomposition
+   * @param jacobian: Jacobian Matrix
+   * @param jacobianInv: Inverse of Jacobian Matrix
+   */
+  void calculate_inverse_jacobian_bySVD( const Eigen::MatrixXd& jacobian, Eigen::MatrixXd& jacobianInv );
 
-		std::vector<KDL::Vector> jnt_rot_axis;
-		//Eigen::MatrixXi axis_of_rotation;	//todo: replace kdl::vector with Eigen vector
-
-		KDL::Chain	chain;
-		std::vector<KDL::Frame>	frames;
-		std::vector<KDL::Joint>	jnts;
-		std::vector<KDL::Frame>	jnt_homo_mat;	//homo matrix of each frame with prevoius joint
-
-		std::vector<double> jnt_pos_min_limit;
-		std::vector<double> jnt_pos_max_limit;
-		std::vector<double> jnt_vel_limit;
-		std::vector<std::string> frame_names;
-		std::vector<std::string> jnts_name;
-		std::vector<geometry_msgs::Vector3> link_length;
-
-		std::vector<KDL::Frame> jnt_fk_mat;	//ff_mat
-		std::vector<KDL::Frame> transformation_matrix;
-		KDL::Frame fk_mat;
-		Eigen::Matrix<double, 6, 7> JacobianMatrix;	//Jacobian Matrix	todo: change 7 with dof
-
-		std::map<std::string, geometry_msgs::PoseStamped> self_collsion_matrix;
-		tf2_ros::StaticTransformBroadcaster static_broadcaster;
+  /**
+   * @brief calculate_inverse_jacobian_bySVD: calculate inverse of Jacobian Matrix using 'Direct Method'
+   * @param jacobian: Jacobian Matrix
+   * @param jacobianInv: Inverse of Jacobian Matrix
+   */
+  void calculate_inverse_jacobian_byDirect( const Eigen::MatrixXd& jacobian, Eigen::MatrixXd& jacobianInv );
 
 
-	    boost::shared_ptr<KDL::ChainFkSolverVel_recursive> jntToCartSolver_vel_;
-	    boost::shared_ptr<KDL::ChainJntToJacSolver> jntToJacSolver_;
-	    boost::shared_ptr<KDL::ChainFkSolverPos_recursive> jntToCartSolver_pos_;
+  /** public data members */
+  // transformation matrix between two concecutive frame
+  //std::vector<Eigen::MatrixXd> Transformation_Matrix_;
+  std::vector<Eigen::Affine3d> Transformation_Matrix_;
 
-		void clear_data_member();
-		void convert_kdl_frame_to_Eigen_matrix(const KDL::Frame& kdl_frame, Eigen::Matrix4d& egn_mat);
-		//void convert_kdl_vec_to_Eigen_vec(const KDL::Vector& kdl_vec, Eigen::VectorXd& egn_mat);
+  // Forward kinematic matrix from root link till current link
+  std::vector<Eigen::MatrixXd> FK_Homogenous_Matrix_;
 
-		void create_transformation_matrix(const uint16_t& segment_number, const double& roll,const double& pitch, const double& yaw);
-		void createRoatationMatrix(const double& angle, const std::vector<unsigned int>& rot_axis, KDL::Frame& lcl_homo_mat);
-
-		// Computation of forward kinematic, jacobian matrix
-		void forward_kinematics(const KDL::JntArray& jnt_angels);
-		void compute_jacobian(const KDL::JntArray& jnt_angels);
-
-		// Computation of forward kinematic, jacobian matrix using kdl library
-		void kdl_forward_kinematics(const KDL::JntArray& jnt_angels);
-		void kdl_compute_jacobian(const KDL::JntArray& jnt_angels);
+  // Forward kinematic matrix represent end effector position relative to root_link
+  Eigen::MatrixXd FK_Matrix_;
 
 
-		//void compute_interia_matrix();
-		//void compute_mass_matrix();
-	public:
+private:
 
-		//Kinematics(const std::string rbt_description = "/robot_description", const std::string& base_link="arm_base_link", const std::string& tip_link="arm_7_link", const std::string& root_frame="world");
+  unsigned int segments_;
 
-		Kinematic_calculations();
-		~Kinematic_calculations();
+  KDL::Chain chain;
+  urdf::Model model;
 
-		bool initialize(const std::string rbt_description_param = "/robot_description", const std::string& base_link_param="arm_base_link", const std::string& tip_link_param="arm_7_link", const std::string& root_frame_param="arm_base_link");
+  /**
+   * @brief initializeDataMember: initialize data member from kinematic chain
+   * @param chain: kinematic chain of robotic description, usually it's full desciption of robots
+   */
+  void initializeDataMember(const KDL::Chain& chain);
 
-		// Calculate inverse of jacobian by using singular value decomposition (SVD), direct method
-		void calculate_inverse_jacobian_bySVD( const Eigen::MatrixXd& jacobian, Eigen::MatrixXd& jacobianInv );
-		void calculate_inverse_jacobian_byDirect( const Eigen::MatrixXd& jacobian, Eigen::MatrixXd& jacobianInv );
+  /**
+   * @brief initializeLimitParameter: initialize joint limits using urdf model
+   * @param model urdf model of robot desciption
+   */
+  void initializeLimitParameter(const urdf::Model& model);
 
-		// Get FK matrix
-		void get_forward_kinematics(KDL::Frame& fk_mat);
-		void get_forward_kinematics(Eigen::MatrixXd& fk_mat);
-		void compute_and_get_FK(const Eigen::VectorXd& jnt_angles, Eigen::Matrix4d& FK_Mat);
-		void compute_and_get_gripper_pose(const Eigen::VectorXd& jnt_angles, Eigen::VectorXd& gripper_pose);
-		KDL::Frame get_forward_kinematics(void);
+  void transformKDLTOEigen(const KDL::Frame& frame, Eigen::MatrixXd& matrix);
 
-		// Get Jacobian matrix
-		void get_jacobian(const KDL::JntArray& jnt_angles, Eigen::MatrixXd& j_mat);
-		void compute_and_get_jacobian(const KDL::JntArray& jnt_angles, Eigen::MatrixXd& j_mat);
-		void compute_and_get_jacobian(const Eigen::VectorXd& jnt_angles, Eigen::MatrixXd& j_mat);
-		Eigen::MatrixXd get_jacobian(const KDL::JntArray& jnt_angles);
+  void clear_data_member();
 
-		// Get position, velocity joint limits
-		void get_joint_limits(const std::string& name_of_limit, std::vector<double>& limit_vec);
-		void get_min_joint_position_limits( std::vector<double>& limit_vec);
-		void get_max_joint_position_limits( std::vector<double>& limit_vec);
-		void get_joint_velocity_limits( std::vector<double>& limit_vec );
-
-		// Get joint and frame names
-		void get_joints_name(std::vector<std::string>& jnts_name_param);
-		void get_frame_names(std::vector<std::string>& frame_names_param);
-
-		// Set joint and frame names
-		void set_joint_names(std::vector<std::string>& jnt_names_param);
-		void set_joint_limits(const std::string& name_of_limit, const std::vector<double>& limit_vec);
-
-		// Set position, velocity joint limits
-		void set_min_joint_position_limits( const std::vector<double>& limit_vec);
-		void set_max_joint_position_limits( const std::vector<double>& limit_vec);
-		void set_joint_velocity_limits( const std::vector<double>& limit_vec );
-
-		// compute current quaternion
-		void compute_and_get_currrent_gripper_poseStamped(const Eigen::VectorXd& jnt_angles, geometry_msgs::PoseStamped& current_quaternion);
-
-		void compute_gripper_pose_and_jacobian(const std::vector<double>& jnt_position, geometry_msgs::PoseStamped& gripper_pose, Eigen::MatrixXd& jacobian_mat);
-
-		/**
-		 * @brief using forward kinematic compute joint poseStamped relatice to root frame
-		 */
-		void compute_and_get_each_joint_pose(const std::vector<double>& jnt_position, std::vector<geometry_msgs::PoseStamped>& each_joint_stamped, std::vector<geometry_msgs::Vector3>& link_length);
-
-		void compute_and_get_each_joint_pose(const std::vector<double>& jnt_position, std::map<std::string, geometry_msgs::PoseStamped>& self_collsion_matrix);
-
-		bool create_static_frame(const geometry_msgs::PoseStamped& stamped, const std::string& frame_name);
-
-		// Debug function for kinematic calculation class
-		void print_data_memebers(void);
-		void print_fk_and_jacobian_matrix(const KDL::JntArray& jnt_angles);
-		void print_kdl_fk_and_jacobian_matrix(const KDL::JntArray& jnt_angles);
-
-	};
+};
 
 #endif //PREDICTIVE_CONTROL_KINEMATIC_CALCULATIONS_H_

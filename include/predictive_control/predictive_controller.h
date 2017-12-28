2cca4345c22f403dcaf1a94ba16c168f54edb25d
ipa-mjp <mayank.jitendrakumar.patel@ipa.fraunhofer.de>
2017-12-28 22:05:00 +0100

    clearing predicitve control

--- include/predictive_control/predictive_controller.h
+++ include/predictive_control/predictive_controller.h
@@ -2,109 +2,136 @@
 #ifndef PREDICTIVE_CONTROL_PREDICTIVE_CONTROLLER_H
 #define PREDICTIVE_CONTROL_PREDICTIVE_CONTROLLER_H
 
-//This file containts read parameter from server, callback, call class objects, control all class
-
 // ros includes
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <sensor_msgs/JointState.h>
-#include <std_srvs/Trigger.h>
 #include <tf/tf.h>
-#include <std_msgs/String.h>
 #include <std_msgs/Float64.h>
 #include <std_msgs/Float64MultiArray.h>
 #include <tf/transform_listener.h>
-#include <tf/transform_datatypes.h>
-#include <std_srvs/Trigger.h>
-#include <visualization_msgs/Marker.h>
-#include <visualization_msgs/MarkerArray.h>
+
+// eigen includes
+#include <Eigen/Eigen>
+#include <Eigen/Core>
+#include <Eigen/LU>
 
 // std includes
-#include <assert.h>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <math.h>
 #include <algorithm>
-#include <iostream>
 
 // boost includes
 #include <boost/shared_ptr.hpp>
-#include <boost/thread/mutex.hpp>
 
 // yaml parsing
 #include <fstream>
 #include <yaml-cpp/yaml.h>
 
 // predicitve includes
-#include <predictive_control/predictive_trajectory_generator.h>
+#include <predictive_control/predictive_configuration.h>
 #include <predictive_control/kinematic_calculations.h>
-#include <cob_srvs/SetString.h>
+#include <predictive_control/collision_detection.h>
 
-class predictive_control_node
+class predictive_control
 {
-private:
-
-	ros::NodeHandle nh;
-	ros::Subscriber joint_state_sub;
-	ros::Publisher joint_velocity_pub;
-
-	 tf::TransformListener tf_listener_;
-
-	// Timer
-    double update_rate_;
-    double cartesian_dist;
-	double rotation_dist;
-
-    ros::Timer timer_;
-    unsigned int dof;
-
-    ros::Publisher marker_pub;
-    std::vector<geometry_msgs::PoseStamped> tranformation_matrix_stamped; 	// transformation matrix convert to poseStamped relative to root link
-
-	// predictive configuration parameter
-	predictive_config new_config;
-
-	// current position and velocity from joint state callback
-	std::vector<double> current_position;
-	std::vector<double> current_velocity;
-
-	// type of variable used to publish joint velocity
-	std_msgs::Float64MultiArray joint_velocity_data;
-	geometry_msgs::PoseStamped current_gripper_pose;
-	geometry_msgs::PoseStamped target_gripper_pose;
-	Eigen::MatrixXd Jacobian_Mat;
-
-	// self collision avoidance
-	//Eigen::MatrixXd collision_distance_matrix;
-	std::vector<double> collision_distance_vector;
-
-	// kinematic calculation
-	boost::shared_ptr<Kinematic_calculations> kinematic_solver_;
-
-	// trajectory generator
-	boost::shared_ptr<pd_frame_tracker> pd_frame_tracker_;
-
-	void spin_node();
-
-	void run_node(const ros::TimerEvent& event);
-
-	void convert_std_To_Eigen_vector(const std::vector<double>& std_vec, Eigen::VectorXd& eigen_vec);
-
-	void publish_zero_jointVelocity();
+  /** Managing of all classes execution of predictive control
+   * - Handle self collsion avoidance
+   * - Extract current position and velocity of manipulator joints
+   * - Publish controlled joint velocity
+   */
 
 public:
 
-	predictive_control_node();
-	~predictive_control_node();
+  /**
+   * @brief predictive_control: Default constructor, allocate memory
+   */
+  predictive_control();
 
-	// Read and initialize data member of predictive_config class from parameter server
-	void read_predictive_parameters(predictive_config& new_param);
+  /**
+   * @brief ~predictive_control: Default distructor, free memory
+   */
+  ~predictive_control();
 
-	// Get current position and velocity at each joint
-	void joint_state_callBack(const sensor_msgs::JointState::ConstPtr& msg);
+  bool initialize();
 
-	void main_predictive_control(void);
+  /**
+   * @brief jointStateCallBack: Get current position and velocity at each joint
+   * @param msg: Read data from sensor_msgs::JointState
+   */
+  void jointStateCallBack(const sensor_msgs::JointState::ConstPtr& msg);
 
+  /**
+   * @brief controlSquence: Known as main control of all classes
+   */
+  void controlSquence(void);
+
+
+  /** public data member */
+  // joint state subsciber to get current joint value
+  ros::Subscriber joint_state_sub_;
+
+  // joint velocity, should be control velocity of controller
+  ros::Publisher joint_velocity_pub_;
+
+private:
+
+  ros::NodeHandle nh;
+
+   tf::TransformListener tf_listener_;
+
+   // degree of freedom
+   uint32_t degree_of_freedom_;
+
+  // Clock frequency
+  double update_rate_;
+
+  // Cartesian distance/error
+  double cartesian_dist_;
+
+  // Rotation distance/error
+  double rotation_dist_;
+
+  // Timmer
+  ros::Timer timer_;
+
+  // Current and last position and velocity from joint state callback
+  Eigen::VectorXd current_position_;
+  Eigen::VectorXd last_position_;
+  Eigen::VectorXd current_velocity_;
+  Eigen::VectorXd last_velocity_;
+
+  // Type of variable used to publish joint velocity
+  std_msgs::Float64MultiArray controlled_velocity;
+
+  // predictive configuration
+  boost::shared_ptr<predictive_configuration> pd_config_;
+
+  // kinematic calculation
+  boost::shared_ptr<Kinematic_calculations> kinematic_solver_;
+
+  // self collision detector/avoidance
+  boost::shared_ptr<CollisionRobot> collision_detect_;
+
+  void spinNode();
+
+  /**
+   * @brief runNode: Continue updating this function depend on clock frequency
+   * @param event: Used for computation of duration of first and last event
+   */
+  void runNode(const ros::TimerEvent& event);
+
+  /**
+   * @brief publishZeroJointVelocity: published zero joint velocity is statisfied cartesian distance
+   */
+  void publishZeroJointVelocity();
+
+  /**
+   * @brief clearDataMember: clear vectors means free allocated memory
+   */
+  void clearDataMember();
 };
 
 #endif
--- src/predictive_controller.cpp
+++ src/predictive_controller.cpp
@@ -4,86 +4,73 @@
 #include <predictive_control/predictive_controller.h>
 #include <boost/thread/thread.hpp>
 
-predictive_control_node::predictive_control_node()
+predictive_control::predictive_control()
 {
-	//nh = "predictive_control_node";
-
-	//nh = ros::this_node::getName();
-
+;
 }
 
-predictive_control_node::~predictive_control_node()
+predictive_control::~predictive_control()
 {
-	current_position.clear();
-	current_velocity.clear();
+  clearDataMember();
+  delete pd_config_;
+  delete kinematic_solver_;
+  delete collision_detect_;
 }
 
-void predictive_control_node::spin_node()
+void predictive_control::spinNode()
 {
 	ROS_INFO(" Predictive control node is running, now it's 'Spinning Node'");
 	ros::spin();
 }
 
-void predictive_control_node::read_predictive_parameters(predictive_config& new_param)
+// diallocated memory
+void predictive_control::clearDataMember()
 {
+  current_position_ = Eigen::VectorXd::resize(0, 0.0);
+  last_position_ = Eigen::VectorXd::resize(0, 0.0);
+  current_velocity_ = Eigen::VectorXd::resize(0, 0.0);
+  last_velocity_ = Eigen::VectorXd::resize(0, 0.0);
+}
 
-	//ros::NodeHandle nh = std::string("arm");
 
-	// Get chain_base and chain tip links, root frame, traget frame
-	if (!nh.getParam ("/chain_base_link", new_param.base_link) )
-	{
-		ROS_WARN(" Parameter 'chain_base_link' not set on %s node " , ros::this_node::getName().c_str());
-	}
+bool predictive_control::initialize()
+{
+  ros::NodeHandle nh_tracker("pd_control");
 
-	if (!nh.getParam ("/chain_tip_link", new_param.tip_link) )
-	{
-		ROS_WARN(" Parameter 'chain_tip_link' not set on %s node " , ros::this_node::getName().c_str());
-	}
+  // make sure node is still running
+  if (ros::ok())
+  {
+    // initialize helper classes, make sure pd_config should be initialized first as mother of other class
+    pd_config_.reset(new predictive_configuration());
+    bool pd_config_success = pd_config_->initialize();
 
-	if (!nh.getParam ("/root_frame", new_param.root_frame) )
-	{
-		ROS_WARN(" Parameter 'root_frame' not set on %s node " , ros::this_node::getName().c_str());
-	}
+    kinematic_solver_.reset(new Kinematic_calculations());
+    bool kinematic_success = kinematic_solver_->initialize();
 
-	if (!nh.getParam ("/target_frame", new_param.target_frame) )
-	{
-		ROS_WARN(" Parameter 'target_frame' not set on %s node " , ros::this_node::getName().c_str());
-	}
+    collision_detect_.reset(new CollisionRobot());
+    bool collision_success = collision_detect_->initializeCollisionRobot();
 
-	//Get joint names
-	if (!nh.getParam ("/joint_names", new_param.jnts_name) )
-	{
-		ROS_WARN(" Parameter 'joint names' not set on %s node " , ros::this_node::getName().c_str());
-	}
+    // check successfully initialization of all classes
+    if (pd_config_success == false || kinematic_success == false
+        || collision_success == false || pd_config_->initialize_success_ == false)
+    {
+      ROS_ERROR("predictive_control: FAILED TO INITILIZED!!");
+      std::cout << "States: \n"
+                << " pd_config: " << std::boolalpha << pd_config_success << "\n"
+                << " kinematic solver: " << std::boolalpha << kinematic_success << "\n"
+                << "collision detect: " << std::boolalpha << collision_success << "\n"
+                << "pd config init success: " << std::boolalpha << pd_config_->initialize_success_
+                << std::endl;
+      return false;
+    }
 
-	new_param.dof = new_param.jnts_name.size();
 
-	// Get debug info, using active_output
-	nh.param("/activate_output", new_param.activate_output, bool(false));
-
-	nh.param("/update_rate", new_param.update_rate, double(50.0));	//hz
-
-	// Get limit parameters and tolerance
-	nh.param("/min_position_limit", new_param.min_position_limit, double(-3.14));
-	nh.param("/max_position_limit", new_param.max_position_limit, double( 3.14));
-	nh.param("/min_velocity_limit", new_param.min_velocity_limit, double( 0.00));
-	nh.param("/max_velocity_limit", new_param.max_velocity_limit, double( 2.00));
-	nh.param("/desired_velocity", new_param.desired_velocity, double(1.5));
-	nh.param("/position_tolerance", new_param.position_tolerance, double(1.0));
-	nh.param("/velocity_tolerance", new_param.velocity_tolerance, double(0.01));
-
-	// Get discretization_steps
-	nh.param("/min_discretization_steps", new_param.min_discretization_steps, int(10));
-	nh.param("/max_discretization_steps", new_param.max_discretization_steps, int(20));
-	nh.param("/discretization_steps", new_param.discretization_steps, int(15));
-
-	ROS_INFO("predictive_control_node:read_predictive_parameters ... is finished");
-	//current_position.resize(new_param.dof, 0.0);
-	//current_velocity.resize(new_param.dof, 0.0);
+  }
 
 }
 
-void predictive_control_node::joint_state_callBack(const sensor_msgs::JointState::ConstPtr& msg)
+
+void predictive_control::joint_state_callBack(const sensor_msgs::JointState::ConstPtr& msg)
 {
 	// Make sure both members are empty before use as used push_back command
 	current_position.clear();	current_velocity.clear();

7927e49cf6a35ab0b0bb66bdb7c045a3848ad65b
ipa-mjp <mayank.jitendrakumar.patel@ipa.fraunhofer.de>
2017-12-29 11:35:57 +0100

    call predictive control class

--- src/predictive_control_node.cpp
+++ src/predictive_control_node.cpp
@@ -1,22 +1,32 @@
 
 #include <ros/ros.h>
-//#include <predictive_control/predictive_controller.h>
+#include <predictive_control/predictive_controller.h>
 
 int main(int argc, char **argv)
 {
-	try
-	{
-		ros::init(argc, argv, ros::this_node::getName());
-    //predictive_control_node pd_control_node;
-    //pd_control_node.main_predictive_control();
-		ros::spin();
-	}
+  try
+  {
+    ros::init(argc, argv, ros::this_node::getName());
+    predictive_control pd_control_;
 
-	catch (ros::Exception& e)
-	{
-		ROS_ERROR("Error occured: %s ", e.what());
-		exit(1);
-	}
+    if (!pd_control_.initialize())
+    {
+      ROS_ERROR_STREAM_NAMED("FILED TO INITIALIZE %s", ros::this_node::getName().c_str());
+      exit(1);
+    }
+    else
+    {
+      // spin node, till ROS node is running on
+      ROS_INFO_STREAM_NAMED("%s INITIALIZE SUCCESSFULLY!!", ros::this_node::getName().c_str());
+      ros::spin();
+    }
+  }
 
+  catch (ros::Exception& e)
+  {
+    ROS_ERROR("predictive_control_node: Error occured: %s ", e.what());
+    exit(1);
+  }
 
+  return 0;
 }

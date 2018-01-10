01aa43840b5c36b7ac7e0eb09015399766f082e2
ipa-mjp <mayank.jitendrakumar.patel@ipa.fraunhofer.de>
2018-01-10 11:34:41 +0100

    change intersection point limit, need to change static collsion cost function

--- src/collision_detection.cpp
+++ src/collision_detection.cpp
@@ -106,10 +106,10 @@ void CollisionRobot::generateCollisionVolume(const std::vector<Eigen::MatrixXd> 
   //for (auto const& it: FK_Homogenous_Matrix)
   for (auto it = FK_Homogenous_Matrix.begin(); it != FK_Homogenous_Matrix.end(); ++it)
   {
-    if (Transformation_Matrix[counter](2,3) > 0.10 && counter != 0)
+    if (Transformation_Matrix[counter](2,3) > 0.15 && counter != 0)
     {
       // distance between two frame are more than ball randius than add intermidate ball
-      if( Transformation_Matrix[counter](2,3) > predictive_configuration::ball_radius_) //0.30
+      if( Transformation_Matrix[counter](2,3) > 0.33) //predictive_configuration::ball_radius_
       {
         ROS_INFO("CollisionRobot: Add intermidiate volume with point: %s", (key+std::to_string(point)).c_str());
 
@@ -136,7 +136,7 @@ void CollisionRobot::generateCollisionVolume(const std::vector<Eigen::MatrixXd> 
         collision_matrix_[key + std::to_string(point)] = stamped;
         point = point + 1;
       }
-    /*
+      /*
       // as usally add ball at every joint
       geometry_msgs::PoseStamped stamped;
       KDL::Frame frame;

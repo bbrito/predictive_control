2e55630b743bb99f892171aa08f2fd1da87821e0
ipa-mjp <mayank.jitendrakumar.patel@ipa.fraunhofer.de>
2018-01-22 13:15:29 +0100

    remove environment function added

--- script/check_add_static_object_service.py
+++ script/check_add_static_object_service.py
@@ -29,7 +29,7 @@ def add_environment():
         request.object_id = "box"
 
         # dimension of object
-        dimension = geometry_msgs.msg.Vector3
+        dimension = geometry_msgs.msg.Vector3()
         dimension.x = 1.30
         dimension.y = 1.30
         dimension.z = 0.10
@@ -55,7 +55,7 @@ def add_environment():
 
         # call service to add static object into environments
         success = client(request)
-        """
+
         if (success):
             rospy.loginfo("Successfully added " + request.object_id + " into environment")
 
@@ -63,12 +63,42 @@ def add_environment():
             rospy.logerr("Failed to add " + request.object_id + " into environment")   
 
         response = predictive_control.srv.StaticCollisionObjectResponse()
-        rospy.loginfo(" Service status ", response.success, " with message ", response.message)
-        """
+        print (response.success)
+        print response.message
+
     except rospy.ServiceException as exc:
             rospy.logerr(" Service did not process request " + str(exc))
+
+
+def remove_environment():
+    rospy.loginfo("Calling static object service ... ")
+    rospy.wait_for_service("/arm/predictive_control/StaticCollision/remove_static_object")
+    rospy.loginfo("Now all services are available ... ")
+
+    try:
+        client = rospy.ServiceProxy("/arm/predictive_control/StaticCollision/remove_static_object",
+                                    predictive_control.srv.StaticCollisionObject)
+        request = predictive_control.srv.StaticCollisionObjectRequest()
+
+        request.object_name = "box"
+        request.object_id = "box"
+
+        # call service to add static object into environments
+        success = client(request)
+
+        if (success):
+            rospy.loginfo("Successfully removed " + request.object_id + " into environment")
+
+        else:
+            rospy.logerr("Failed to remove " + request.object_id + " into environment")
+
+
+    except rospy.ServiceException as exc:
+        rospy.logerr(" Service did not process request " + str(exc))
 
 
 if __name__ == '__main__':
     rospy.init_node("pd_static_object_service")
     add_environment()
+    rospy.sleep(3.0)
+    remove_environment()

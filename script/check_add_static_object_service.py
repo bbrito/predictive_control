f42d714fae9fd91bdf4022cf8740c6afd7036019
ipa-mjp <mayank.jitendrakumar.patel@ipa.fraunhofer.de>
2018-01-23 14:42:37 +0100

    add boject into list

--- script/check_add_static_object_service.py
+++ script/check_add_static_object_service.py
@@ -30,9 +30,9 @@ def add_environment(object_id):
 
         # dimension of object
         dimension = geometry_msgs.msg.Vector3()
-        dimension.x = 1.30
-        dimension.y = 1.30
-        dimension.z = 0.10
+        dimension.x = 0.40
+        dimension.y = 0.60
+        dimension.z = 0.40
         request.dimension = dimension
 
         # set object position and orientation
@@ -41,9 +41,9 @@ def add_environment(object_id):
         pose.header.stamp = rospy.Time().now()
         
         # position
-        pose.pose.position.x = 0.0
-        pose.pose.position.y = 0.0
-        pose.pose.position.z = 0.05
+        pose.pose.position.x = 0.30 + dimension.x * 0.5
+        pose.pose.position.y = -0.30 + dimension.y * 0.5
+        pose.pose.position.z = 0.00 + dimension.z * 0.5
 
         # object orientation
         pose.pose.orientation.w = 1.0
@@ -68,6 +68,56 @@ def add_environment(object_id):
 
     except rospy.ServiceException as exc:
             rospy.logerr(" Service did not process request " + str(exc))
+
+
+def add_environment_from_file(object_id):
+    rospy.loginfo("Calling static object service ... ")
+    rospy.wait_for_service("/arm/predictive_control/StaticCollision/add_static_object")
+    rospy.loginfo("Now all services are available ... ")
+
+    try:
+        client = rospy.ServiceProxy("/arm/predictive_control/StaticCollision/add_static_object",
+                                    predictive_control.srv.StaticCollisionObject)
+        request = predictive_control.srv.StaticCollisionObjectRequest()
+
+        request.object_name = object_id
+        request.object_id = object_id
+
+        request.file_name = object_id
+
+        # set object position and orientation
+        pose = geometry_msgs.msg.PoseStamped()
+        pose.header.frame_id = "world"
+        pose.header.stamp = rospy.Time().now()
+
+        # position
+        pose.pose.position.x = 0.50
+        pose.pose.position.y = 0.0
+        pose.pose.position.z = 0.00
+
+        # object orientation
+        pose.pose.orientation.w = 1.0
+        pose.pose.orientation.x = 0.0
+        pose.pose.orientation.y = 0.0
+        pose.pose.orientation.z = 0.0
+
+        request.primitive_pose = pose
+
+        # call service to add static object into environments
+        success = client(request)
+
+        if (success):
+            rospy.loginfo("Successfully added " + request.object_id + " into environment")
+
+        else:
+            rospy.logerr("Failed to add " + request.object_id + " into environment")
+
+        response = predictive_control.srv.StaticCollisionObjectResponse()
+        print (response.success)
+        print response.message
+
+    except rospy.ServiceException as exc:
+        rospy.logerr(" Service did not process request " + str(exc))
 
 
 def remove_environment(object_id):
@@ -102,7 +152,7 @@ if __name__ == '__main__':
     rospy.init_node("pd_static_object_service")
 
     object_id = "box"
-
+    #add_environment_from_file(object_id=object_id)
     add_environment(object_id)
-    rospy.sleep(3.0)
-    remove_environment(object_id)
+    #rospy.sleep(3.0)
+    #remove_environment(object_id)

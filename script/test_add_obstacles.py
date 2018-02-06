71bde67c3d9f115cc31453c1d426ac353b24f87d
ipa-mjp <mayank.jitendrakumar.patel@ipa.fraunhofer.de>
2018-02-06 16:11:48 +0100

    change name of object

--- script/test_add_obstacles.py
+++ script/test_add_obstacles.py
@@ -107,13 +107,46 @@ class StaticObstacles:
         print (response.success)
         print (response.message)
 
+    def add_environment_from_file(self, object_id):
+        rospy.loginfo("Calling static object service ... ")
+        rospy.wait_for_service("/arm/pd_control/add_static_obstacles")
+        rospy.loginfo("Now all services are available ... ")
+
+        try:
+            client = rospy.ServiceProxy("/arm/pd_control/add_static_obstacles", predictive_control.srv.StaticObstacle)
+            request = predictive_control.srv.StaticObstacleRequest()
+            response = predictive_control.srv.StaticObstacleResponse()
+
+            co = moveit_msgs.msg.CollisionObject()
+
+            # at this moment, object_id same as frame of object
+            co.id = object_id
+
+            request.static_collision_object = co
+            request.file_name = "bookshelves"
+
+            # call service to add static object into environments
+            success = client(request)
+
+            if (success):
+                rospy.loginfo("Successfully added " + object_id + " into environment")
+
+            else:
+                rospy.logerr("Failed to add " + object_id + " into environment")
+
+            print (response.success)
+            print response.message
+
+        except rospy.ServiceException as exc:
+            rospy.logerr(" Service did not process request " + str(exc))
 
 
 if __name__ == '__main__':
     rospy.init_node("pd_static_obstacles_service")
 
-    object_id = "sphere"
+    object_id = "bookshelves"
     OBJECT = StaticObstacles()
-    OBJECT.add_static_obstacles(object_id=object_id)
-    rospy.sleep(15.0)
-    OBJECT.delete_static_obstacles(object_id=object_id)
+    OBJECT.add_environment_from_file(object_id=object_id)
+    #OBJECT.add_static_obstacles(object_id=object_id)
+    #rospy.sleep(15.0)
+    #OBJECT.delete_static_obstacles(object_id=object_id)

a6964ab0c0476d51e81876685f5adab1ff3a3005
Laura Ferranti <laura.ferranti1986@gmail.com>
2018-09-14 13:21:39 +0200

    new acado files with shorter horizon

--- include/predictive_control/generated_mpc/acado_qpoases_interface.hpp
+++ include/predictive_control/generated_mpc/acado_qpoases_interface.hpp
@@ -37,11 +37,11 @@
  */
 
 /** Maximum number of optimization variables. */
-#define QPOASES_NVMAX      75
+#define QPOASES_NVMAX      60
 /** Maximum number of constraints. */
-#define QPOASES_NCMAX      225
+#define QPOASES_NCMAX      180
 /** Maximum number of working set recalculations. */
-#define QPOASES_NWSRMAX    900
+#define QPOASES_NWSRMAX    720
 /** Print level for qpOASES. */
 #define QPOASES_PRINTLEVEL PL_NONE
 /** The value of EPS */
